460662210fbc081adfee8f212238fca94f1b465b
kt alex <17661357211@163.com>
2026-03-07 18:18:54 +0800

    前端vue服务器配置

--- web/vue.config.js
+++ web/vue.config.js
@@ -7,10 +7,10 @@ module.exports = defineConfig({
     port: 8081, // 设置端口
     proxy: {
       '/api': {
-        target: 'http://localhost:10215', // 要请求的后端地址
+        target: 'http://123.56.248.17:10215', // 要请求的后端地址
         changeOrigin: true,
         /*pathRewrite: {
-          '^/api': '', // 重写路径，如果你的请求路径是 '/api/xxx'，实际会去请求 'http://localhost:10215/xxx'
+          '^/api': '', // 重写路径，如果你的请求路径是 '/api/xxx'，实际会去请求 'http://123.56.248.17:10215/xxx'
         },*/
       },
     },
