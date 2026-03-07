15f0f09c0fa5be086c2ef178a33b9c58234e3339
alaner28 <2825005675@qq.com>
2026-03-07 12:11:51 +0800

    1111

--- web/src/config/config.ts
+++ web/src/config/config.ts
@@ -1,5 +1,5 @@
 export const baseUrl = 'http://localhost:10215';
-export const algorithmUrl = 'http://172.17.0.2:6006';
+export const algorithmUrl = 'http://localhost:6006';
 export const webSocketBaseUrl = 'ws://localhost:10215';
 export const agentBaseUrl = 'http://localhost:5050';
-export const rtmpAddress = 'http://127.0.0.1:8080/live/raw.flv';
+export const rtmpAddress = 'http://123.56.248.17:8080/live/raw.flv';
